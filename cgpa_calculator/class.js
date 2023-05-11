db57c806974d9d87ced22b526cb4f19cf00356ab
bensonibeabuchi <ibeabuchibenson@gmail.com>
2023-05-11 13:50:22 +0100

    updated my cgpa calcultaor

---
+++ cgpa_calculator/class.js
@@ -0,0 +1,114 @@
+cap ='abcdefghijklmnopqrstuvwxyz'
+cap = cap.split('')
+console.log(cap)
+
+let obj = {}
+
+let instructor = {
+    name: 'franc',
+    age: 88,
+    town: 'Anambra',
+    likes: ['footbal', 'tennis', 'stories']
+}
+
+instructor.name
+console.log(instructor.name)
+
+console.log(instructor.likes[0])
+
+instructor['town'] = 'Umuchu'
+console.log(instructor)
+
+const frontEnd6b2 = [
+    {
+        firstName: 'benson',
+        surname: 'ibeabuchi',
+        dob: 'june 03',
+        stateOfOrigin: 'abia',
+        hobbies: ['art', 'photography', 'programming', 'travelling'],
+        gender: 'cis-male'
+
+    },
+    {
+        firstName: 'stephen',
+        surname: 'eromosele',
+        dob: 'june 12',
+        stateOfOrigin: 'edo',
+        hobbies: ['gaming', 'programming', 'movies'],
+        gender: 'male'
+
+    },
+    {
+        firstName: 'Daniel',
+        surname: 'Ebuka',
+        dob: 'aug 31',
+        stateOfOrigin: 'anambra',
+        hobbies: ['coding', 'photography', 'browsing'],
+        gender: 'male'
+
+    },
+    {
+        firstName: 'sharon',
+        surname: 'ibalga',
+        dob: 'dec 28',
+        stateOfOrigin: 'akwa-ibom',
+        hobbies: ['eating', 'sleeping', 'coding'],
+        gender: 'female'
+
+    },
+    {
+        firstName: 'samiat',
+        surname: 'usman',
+        dob: 'april 21',
+        stateOfOrigin: 'kwara',
+        hobbies: ['sleeping', 'exploring', 'programming'],
+        gender: 'female'
+    },
+    {
+        firstName: 'chidinma',
+        surname: 'nwaneti',
+        dob: 'july 14',
+        stateOfOrigin: 'anambra',
+        hobbies: ['swimming', 'reading', 'football'],
+        gender: 'female'
+
+    },
+    {
+        firstName: 'thariwat',
+        surname: 'umar',
+        dob: 'april 16',
+        stateOfOrigin: 'jigawa',
+        hobbies: ['swimming', 'reading', 'football'],
+        gender: 'female'
+
+    },
+    {
+        firstName: 'kelubia',
+        surname: 'urubusi',
+        dob: 'march 8',
+        stateOfOrigin: 'delta',
+        hobbies: ['gaming', 'swimming', 'dancing'],
+        gender: 'LGBTQ'
+
+    },
+    {
+        firstName: 'damola',
+        surname: 'gbenga',
+        dob: 'nov 18',
+        stateOfOrigin: 'ekiti',
+        hobbies: ['exploring', 'reading', 'football'],
+        gender: 'LGBTQ'
+
+    },
+    {
+        firstName: 'ameera',
+        surname: 'olusesi',
+        dob: 'april 24',
+        stateOfOrigin: 'lagos',
+        hobbies: ['swimming', 'reading', 'football'],
+        gender: 'LGBTQ'
+
+    },
+
+
+]
